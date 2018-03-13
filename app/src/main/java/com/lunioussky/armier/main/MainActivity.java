2c4fbd78b9f57b9aecf50a233e14184eec519f72
lunious <1164503275@qq.com>
2018-03-13 16:18:06 +0800

    底部虚拟按键适配

--- app/src/main/java/com/lunioussky/armier/main/MainActivity.java
+++ app/src/main/java/com/lunioussky/armier/main/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
